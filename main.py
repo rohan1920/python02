3aec84d8a8a5976bef9a859fb3f7346c25cf7617
rohan1920 <talalmajeed85@gmail.com>
2024-04-01 22:26:46 -0700

    python 02

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+def hello_world():
+    return "Hello, world!"
+
+class MyClass:
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        return f"Hello, {self.name}!"
